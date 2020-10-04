1f440fc0e86c5de3b11f0de536aed9d7b3e33353
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-05 00:32:16 +0700

    Add files via upload

--- src/component/PageBreadcrumb.js
+++ src/component/PageBreadcrumb.js
@@ -6,7 +6,7 @@ function PageBreadcrumb(){
         <div>
             <Breadcrumb>
                 <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-                <Breadcrumb.Item href="https://github.com/syaifulfahmi/tugas_9_react_js_code">Liga Inggris</Breadcrumb.Item>
+                <Breadcrumb.Item href="https://github.com/syaifulfahmi/tugas_9_react_js_code" target="blank" >Liga Inggris</Breadcrumb.Item>
                 <Breadcrumb.Item active>Transfer Pemain</Breadcrumb.Item>
             </Breadcrumb>
         </div>
